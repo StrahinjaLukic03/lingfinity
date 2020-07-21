e28dccef89485d0e7a35984dec71ca63d5374377
ampir3 <ampir3@bakabois.eu>
2020-07-21 10:11:14 +0200

    read Description
    - res directory created for media & misc.
    - Discord button got added in res/
    - It changes its color according to the theme

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ function Choice(elem){
             document.getElementById("header").style.color = "black";
             document.getElementById("dTbutton").className = "btn btn-dark";
             document.getElementById("contButton").className = "btn btn-outline-dark";
+            document.getElementById("discord").src = "res/Discord-Logo-Black.png";
         }
         else{
             elem.firstChild.data = "Light Theme";
             document.getElementById("header").style.color = "#d1d1d1";
             document.getElementById("dTbutton").className = "btn btn-outline-light";
             document.getElementById("contButton").className = "btn btn-outline-light";
+            document.getElementById("discord").src = "res/Discord-Logo-White.png";
         }
     };
 };
