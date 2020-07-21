38504007dc0d40c9ff1d68eaf8c766a5fad496f4
ampir3 <ampir3@bakabois.eu>
2020-07-21 16:52:37 +0200

    slider instead of button

---
+++ index.js
@@ -0,0 +1,17 @@
+function Choice(checkbox){
+	var element = document.body;
+	if(checkbox.checked){
+		element.className = "dark-mode";
+		document.getElementById("header").style.color = "#d1d1d1";
+		document.getElementById("contButton").className = "btn btn-outline-light";
+		document.getElementById("discord").src = "res/Discord-Logo-White.png";
+		document.getElementsByClassName("slider round").style.color = "black";
+	}
+
+	else{
+		element.className = "light-mode";
+		document.getElementById("header").style.color = "black";
+		document.getElementById("contButton").className = "btn btn-outline-dark";
+		document.getElementById("discord").src = "res/Discord-Logo-Black.png";
+	}
+}
