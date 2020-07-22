2dfc0d4597bfbdf4aef7b258e3d336b584306546
ampir3 <ampir3@bakabois.eu>
2020-07-22 10:20:13 +0200

    bar invert implemented

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 //Switch logic
+var box = null;
+var bullets = ["bullet1", "bullet2", "bullet3", "bullet4"];
+
+
 function Choice(checkbox){
 	var element = document.body;
-	var bar = document.getElementById("bar");
-	
+	box = checkbox;
 	if(checkbox.checked){
 		element.className = "dark-mode";
 		document.getElementById("header").style.color = "#d1d1d1";
 		document.getElementById("contButton").className = "btn btn-outline-light";
 		document.getElementById("modeImg").src = "res/moon.png";
-		//TODO: Finish the bar invert!
-		//document.getElementById("modeImg").style.filter = "invert(100%)";
-		//document.getElementById("bar").style.backgroundColor = "black";
-		//bar.getElementsByTagName("a").style.color = "white";
-
+		document.getElementById("modeImg").style.filter = "invert(100%)";
+		document.getElementById("bar").style.backgroundColor = "black";
+		bullets.forEach(switchColor);
 	}
 
 	else{
@@ -20,7 +21,20 @@ function Choice(checkbox){
 		document.getElementById("header").style.color = "black";
 		document.getElementById("contButton").className = "btn btn-outline-dark";
 		document.getElementById("modeImg").src = "res/sun.png";
-		//document.getElementById("bar").style.backgroundColor = "white";
-		//document.getElementById("bar").style.color = "grey";
+		document.getElementById("modeImg").style.filter = "invert(0%)";
+		document.getElementById("bar").style.backgroundColor = "white";
+		
+		bullets.forEach(switchColor);
+	}
+}
+
+function switchColor(bulletName){
+	var x = document.getElementById(bulletName);
+
+	if(box.checked){
+		x.style.color = "white";
+	}
+	else{
+		x.style.color = "black";
 	}
 }
