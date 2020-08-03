fb4f1e1967f9e8fcc1af740fb6819bf6ca4713da
StrahinjaLukic03 <68552692+StrahinjaLukic03@users.noreply.github.com>
2020-08-03 02:28:42 +0200

    Hell naw

---
+++ scripts/themeLocalStorage.js
@@ -0,0 +1,12 @@
+let darkMode = localStorage.getItem("dark-mode"); // most likely wrong under the quotes
+const darkModeToggle = document.querySelector("#dark-mode"); // most likely wrong under the quotes
+
+darkModeToggle.addEventListener("click", () => {
+    console.log("test");
+})
+
+
+
+// My brain is absolutely fucked... This all is on the link below
+// https://www.youtube.com/watch?v=wodWDIdV9BY
+// (Hold ctrl & left click the link to open it right away in the browser you bastard)
