71858ac15fc212e6057cf2bcef7038544d329f79
StrahinjaLukic03 <68552692+StrahinjaLukic03@users.noreply.github.com>
2020-09-19 21:37:48 +0200

    Commeted somehow navbaranimation.js

--- scripts/navbaranimation.js
+++ scripts/navbaranimation.js
@@ -1,9 +1,10 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 var prevScrollpos = window.pageYOffset;
 var currentScrollPos;
 var barID = "bar";
 var checcc = false;
 
+// This is confusing pls explain
 window.onscroll = function() {
   
   currentScrollPos = window.pageYOffset;
@@ -19,7 +20,7 @@ window.onscroll = function() {
   }
    
 }
-
+// hmmm
 function changeName(){
   if(checcc){
     barID = "bar";
