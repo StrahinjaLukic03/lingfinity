31df19ab85e0db7a9424a37c6215ab1f3267bdea
StrahinjaLukic03 <68552692+StrahinjaLukic03@users.noreply.github.com>
2020-11-15 17:45:12 +0100

    Strahinja's test course is now functionable

--- courses/testStrahinja/script.js
+++ courses/testStrahinja/script.js
@@ -1,13 +1,43 @@
-function Help(tekst) {
-    var extractedVal = document.getElementsByClassName(tekst).value;
-    if(extractedVal == "tekst")
-        document.getElementByClassName(extractedVal).style.borderColor = "#07e04c";
+function Check() {
+    var sam = document.getElementById("sam").value;
+    if(sam == "sam")
+        document.getElementById("sam").style.borderColor = "green";
     else
-        document.getElementByClassName(extractedVal).style.borderColor = "red";
+        document.getElementById("sam").style.borderColor = "red";
 
-    return tekst;
-}
+    var ti = document.getElementById("ti").value;
+    if(ti == "ti")
+        document.getElementById("ti").style.borderColor = "green";
+    else
+        document.getElementById("ti").style.borderColor = "red";
 
-function Check(tekst) {
-    Help(tekst);
+    var se = document.getElementById("se").value;
+    if(se == "se")
+        document.getElementById("se").style.borderColor = "green";
+    else
+        document.getElementById("se").style.borderColor = "red";
+
+    var si = document.getElementById("si").value;
+    if(si == "si")
+        document.getElementById("si").style.borderColor = "green";
+    else
+        document.getElementById("si").style.borderColor = "red";
+    
+    var sam2 = document.getElementById("sam2").value;
+    if(sam2 == "sam")
+        document.getElementById("sam2").style.borderColor = "green";
+    else
+        document.getElementById("sam2").style.borderColor = "red";
+        
+    var ti2 = document.getElementById("ti2").value;
+    if(ti2 == "ti")
+        document.getElementById("ti2").style.borderColor = "green";
+    else
+        document.getElementById("ti2").style.borderColor = "red";
+       
+    var sam3 = document.getElementById("sam3").value;
+    if(sam3 == "sam")
+        document.getElementById("sam3").style.borderColor = "green";
+    else
+        document.getElementById("sam3").style.borderColor = "red";
 }
