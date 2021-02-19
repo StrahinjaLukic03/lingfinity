28c0cf13824736b0a734e46fea5ece20f36e2b0a
Strahinja <68552692+StrahinjaLukic03@users.noreply.github.com>
2021-02-19 20:48:37 +0100

    Added the selected button in course template 2

--- courses/templates/template2/script.js
+++ courses/templates/template2/script.js
@@ -1,3 +1,19 @@
+//changed the style of buttons when they're selected and assigns the class to it
+document.addEventListener('DOMContentLoaded', () => {
+
+    let myBtns = document.querySelectorAll('.buttons2');
+    myBtns.forEach(function(btn) {
+
+        btn.addEventListener('click', () => {
+          myBtns.forEach(b => b.classList.remove('buttons2Clicked'));
+          btn.classList.add('buttons2Clicked');
+        });
+ 
+    });
+
+});
+
+//useless code for now
 function Check() {
     //this is the only this that will work vv
     document.getElementById("panelMessage").innerText = "Correct!";
