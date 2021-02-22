27debdd475313aa4561361d6bf0afe87c86cc72b
Strahinja <68552692+StrahinjaLukic03@users.noreply.github.com>
2021-02-22 12:39:22 +0100

    Made a JSON file

--- courses/templates/template2/script.js
+++ courses/templates/template2/script.js
@@ -1,3 +1,13 @@
+//fetching questions form the JSON file and previewing them in the console
+fetch("questions.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+//here should be the part of code which makes the questions and answers dynamic
+
+
 //changed the style of buttons when they're selected and assigns the class to it
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -16,5 +26,5 @@ document.addEventListener('DOMContentLoaded', () => {
 //useless code for now
 function Check() {
     //this is the only this that will work vv
-    document.getElementById("panelMessage").innerText = "Correct!";
+    //document.getElementById("panelMessage").innerText = "Correct!";
 }
